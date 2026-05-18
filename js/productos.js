e8f0918c0d8e6d169bf37294fe5ef641fdd1f7be
Gabriel-Natividad <Gabodelacruz2006@gmail.com>
2026-05-18 12:01:02 -0600

    Se actualizaron los productos

--- js/productos.js
+++ js/productos.js
@@ -14,7 +14,7 @@ const productos = [
         categoria: "tuberia",
         descripcion: "Tubería con diámetros ½” a 16” en todos los espesores...",
         imagen: "images/productos/tuberia-inox.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha2.pdf"
     },
     {
         id: 3,
@@ -22,7 +22,7 @@ const productos = [
         categoria: "tuberia",
         descripcion: "Conexión forjada en acero al carbón, ASTM SA 105N...",
         imagen: "images/productos/tuberias-seambles.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha3.pdf"
     },
     {
         id: 4,
@@ -30,7 +30,7 @@ const productos = [
         categoria: "conexiones",
         descripcion: "Conexion de acero al carbon para soldar fabricadas bajo normas espicificas...",
         imagen: "images/productos/tuberia-api.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha3.pdf"
     },
 
     // CONEXIONES (5 productos)
@@ -40,7 +40,7 @@ const productos = [
         categoria: "conexiones",
         descripcion: "Conexiones soldables a tope bw astm a-403; t-316; y t-304 con y sin costura..",
         imagen: "images/productos/conexiones-forjadas.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha6.pdf"
     },
     {
         id: 6,
@@ -48,7 +48,7 @@ const productos = [
         categoria: "valvulas",
         descripcion: "Linea completa de de valvulas para sistemas de instrumentacion, tales como valvulas de bola, aguja, check y manifolds...",
         imagen: "images/productos/tubing.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha11.pdf"
     },
     {
         id: 7,
@@ -56,7 +56,7 @@ const productos = [
         categoria: "conexiones",
         descripcion: "Conectores para fubing con doble ferula, conexiones roscas y conectores...",
         imagen: "images/productos/bridas.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/tubing.pdf"
     },
     {
         id: 8,
@@ -64,7 +64,7 @@ const productos = [
         categoria: "tuberia",
         descripcion: "Tubing en acero inoxidable y aleaciones de diametro de 0.250 a 750, fabricado en materiales...",
         imagen: "images/productos/conexiones-instrumentacion.jpg",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/tubing.pdf"
     },
     {
         id: 9,
@@ -72,7 +72,7 @@ const productos = [
         categoria: "conexiones",
         descripcion: "Las tuercas union de golpes estan diseñadas para ser conectores de linea de uso rudo y fljos..",
         imagen: "images/productos/conexion-hammber.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha8.pdf"
     },
 
     // VÁLVULAS (3 productos)
@@ -82,7 +82,7 @@ const productos = [
         categoria: "valvulas",
         descripcion: "Cople con costura cédula 40 negro y galvanizado, diam. de ¼” a 6“ y 8 a 16” ...",
         imagen: "images/productos/valvula-compuerta.png",
-       pdf: "pdf/docs/ficha.pdf"
+       pdf: "pdf/docs/ficha9.pdf"
     },
     {
         id: 11,
@@ -90,7 +90,7 @@ const productos = [
         categoria: "valvulas",
         descripcion: "Acero al carbón forjado, astm a-105 de 3000# a 6000# pregunte por otras opciones ...",
         imagen: "images/productos/valvula-globo.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha9.pdf"
     },
     {
         id: 12,
@@ -98,7 +98,7 @@ const productos = [
         categoria: "valvulas",
         descripcion: "Empaques de anillos para bridas (rtj) de acero inoxidable tipo 316 en forma oval y octagonal...",
         imagen: "images/productos/valvula-bola.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/anillos.pdf"
     },
 
     // ACCESORIOS (3 productos)
@@ -108,7 +108,7 @@ const productos = [
         categoria: "accesorios",
         descripcion: "Espárragos en acero astm a193 gr b7 en diámetros de 3/8” a 3” en distintas longitudes con tuercas,...",
         imagen: "images/productos/accesorio.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/esparrago.pdf"
     },
     {
         id: 14,
@@ -116,7 +116,7 @@ const productos = [
         categoria: "accesorios",
         descripcion: "En clase 150#, 300#, 600# y 900# rf, cumplen con el ansi b 16.5 para diámetros de ½” – 24” y ansi b16.47 ...",
         imagen: "images/productos/accesorio-brida.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha11.pdf"
     },
     {
         id: 15,
@@ -124,7 +124,7 @@ const productos = [
         categoria: "valvula",
         descripcion: "Con cuerpo de hierro fundido, hierro ductil, acero fundido a-216 wcb y acero inoxidable t-316 cf8m.",
         imagen: "images/productos/juntas.png",
-        pdf: "pdf/docs/ficha.pdf"
+        pdf: "pdf/docs/ficha11.pdf"
     }
 ];
 
